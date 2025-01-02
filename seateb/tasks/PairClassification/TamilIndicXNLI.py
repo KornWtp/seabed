50a0ffb6bd8b560bbd9967e649678cb10ee3a382
KornWtp <elecwpr@gmail.com>
2025-01-02 05:19:01 +0000

    Update pair classification task

--- seateb/tasks/PairClassification/TamilIndicXNLI.py
+++ seateb/tasks/PairClassification/TamilIndicXNLI.py
@@ -1,12 +1,12 @@
 from ...abstasks.AbsTaskPairClassification import AbsTaskPairClassification
 
 
-class TamilXNLI(AbsTaskPairClassification):
+class TamilIndicXNLI(AbsTaskPairClassification):
     @property
     def description(self):
         return {
-            "name": "TamilXNLI",
-            "hf_hub_name": "kornwtp/ta-xnli",
+            "name": "TamilIndicXNLI",
+            "hf_hub_name": "kornwtp/ta-indicxnli",
             "description": "Sentence pairs classification similar to existing XNLI dataset in shape/form, but focusses on Indic language family.",
             "reference": "https://huggingface.co/datasets/Divyanshu/indicxnli",
             "category": "s2s",
