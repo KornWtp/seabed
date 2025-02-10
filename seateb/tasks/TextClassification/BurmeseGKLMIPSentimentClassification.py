b3e09ca52fbbfc30ab1b575654d61aec33333d4c
KornWtp <elecwpr@gmail.com>
2025-02-10 05:37:09 +0000

    update huggingface hub name

--- seateb/tasks/TextClassification/BurmeseGKLMIPSentimentClassification.py
+++ seateb/tasks/TextClassification/BurmeseGKLMIPSentimentClassification.py
@@ -6,7 +6,7 @@ class BurmeseGKLMIPSentimentClassification(AbsTaskTextClassification):
     def description(self):
         return {
             "name": "BurmeseGKLMIPSentimentClassification",
-            "hf_hub_name": "kornwtp/Khine-myanmar-news-classification",
+            "hf_hub_name": "kornwtp/burmese-gklmip-sentiment",
             "description": "Myanmar news corpus is intended for training and evaluation of text classification tasks for the Myanmar language.",
             "reference": "https://github.com/GKLMIP/Pretrained-Models-For-Myanmar",
             "category": "s2s",
