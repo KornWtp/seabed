0e95860fc743c67e7857775b32c1bd0c8c274f78
KornWtp <elecwpr@gmail.com>
2025-03-19 14:02:58 +0000

    update name

--- seateb/tasks/QARetrieval/Microbiology1K_mya_QARetrieval.py
+++ seateb/tasks/QARetrieval/Microbiology1K_mya_QARetrieval.py
@@ -5,7 +5,7 @@ class Microbiology1K_ind_QARetrieval(AbsTaskQARetrieval):
     @property
     def description(self):
         return {
-            "name": "Microbiology1K_ind_QARetrieval",
+            "name": "Microbiology1K_mya_QARetrieval",
             "hf_hub_name": "kornwtp/microbiology1k-mya-qaretrieval",
             "description": "Microbiology 1K QA pairs in Burmese Language",
             "reference": "https://huggingface.co/datasets/jojo-ai-mst/Burmese-Microbiology-1K",
