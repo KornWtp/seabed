cc8cdfb976a73cf1b628e5e667351002b9515f97
KornWtp <44723261+KornWtp@users.noreply.github.com>
2025-08-10 09:31:20 +0700

    Update __init__.py

--- seabed/tasks/PairClassification/__init__.py
+++ seabed/tasks/PairClassification/__init__.py
@@ -14,3 +14,4 @@ from .XNLITranslated_khm_PairClassification import *
 from .XNLITranslated_zsm_PairClassification import *
 from .XNLITranslated_lao_PairClassification import *
 from .BurmeseXNLI_mya_PairClassification import *
+from .PAWS_fil_PairClassification import *
