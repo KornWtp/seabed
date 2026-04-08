bba9e182ef8c88c5d394e6acf104b7fde59c9325
KornWtp <44723261+KornWtp@users.noreply.github.com>
2026-04-08 10:31:00 +0700

    Update file

--- seabed/tasks/Classification/ThuraMyanmarNews_mya_Classification.py
+++ seabed/tasks/Classification/ThuraMyanmarNews_mya_Classification.py
@@ -10,7 +10,7 @@ class ThuraMyanmarNews_mya_Classification(AbsTaskClassification):
             "description": "Myanmar news corpus is intended for training and evaluation of text classification tasks for the Myanmar language.",
             "reference": "https://huggingface.co/datasets/ThuraAung1601/myanmar_news",
             "category": "s2s",
-            "type": "TextClassification",
+            "type": "Classification",
             "eval_splits": ["test"],
             "eval_langs": ["mya"],
             "main_score": "f1",
